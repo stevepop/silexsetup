048b326d6045f143a83a30adfe99084961a5c9d7
Steve <steve.popoola@simplestream.com>
2012-09-21 15:55:35 +0100

    Modified the app bootstrap to configure an instance of the UrlService Modified the urlService to parse ini file and get the urls for the passed in short urls.

--- src/Acme/UrlService.php
+++ src/Acme/UrlService.php
@@ -2,16 +2,49 @@
 namespace Acme;
 
 class UrlService {
-    private $urls = array(
-        'goog' => 'http://www.google.com',
-        'fb' => 'http://www.facebook.com',
-    );
+    private $url_list = array();
+    private $url_file = '';
+    const url_regex = '^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?^';
 
-    public function get($short){
-        return $this->urls[$short];
+    public function __construct($url_file_name) {
+        $this->url_file = $url_file_name;
+        $url_file = \parse_ini_file($url_file_name);
+
+        foreach ($url_file as $slug => $url) {
+            $this->url_list[$slug] = $url;
+        }
     }
 
+    public function getUrlSlug()
+    {
+        return $this->url_file;
+    }
+
+    public function get($url_slug) {
+        return $this->url_list[$url_slug];
+    }
+
+    public function add($url_slug, $url) {
+        if (!\preg_match(self::url_regex, $url)) {
+            throw new \Exception('Invalid url');
+        }
+        if (isset($this->url_list[$url_slug])) {
+            throw new \Exception('Url short name already exists');
+        }
+        $this->url_list[$url_slug] = $url;
+        $this->dump();
+    }
+
+    private function dump() {
+        $fh = fopen($this->url_file, 'w');
+        foreach ($this->url_list as $url_slug => $url) {
+            fwrite($fh, $url_slug . ' = ' . $url . "\n");
+        }
+        fclose($fh);
+    }
+
+
     public function getAll(){
-        return $this->urls;
+        return $this->url_list;
     }
 }
--- src/app.php
+++ src/app.php
@@ -14,11 +14,25 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.options' => array('cache' => __DIR__.'/../cache'),
 
 ));
+$app['key'] = 'my_key';
+$app['inifile'] = __DIR__ . '/../resources/urls.ini';
 
-
-$app['url_service'] = function() {
-    return new UrlService();
+$app['url_service'] = function() use ($app) {
+    return new UrlService($app['inifile']);
 };
+
+//$app->register(new \Acme\UrlService(), array('inifile' => __DIR__ . '/../resources/urls.ini'));
+
+$app->get('/add/{key}/{url_slug}', function($url_slug, $key) use ($app){
+    if($app['key'] != $key){
+        throw new Exception('Invalid key');
+    }
+    $app['url_service']->add($url_slug, $app['request']->get('url'));
+    return $app['twig']->render('add.html.twig', array(
+        'url_slug'  =>  $url_slug,
+        'url'  =>  $app['request']->get('url')));
+});
+
 
 $app->get('/{url_slug}',function($url_slug) use($app){
     return $app->redirect($app['url_service']->get($url_slug));
